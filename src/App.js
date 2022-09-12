cdc29b846c278f4dfc99c4037138d0b08d58cf30
Yunus Taş <dolphinstone34@gmail.com>
2022-09-12 14:48:35 +0300

    light-dark-theme

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import "./index.css"
+import useLocalStorage from 'use-local-storage';
+
+
+
+
+
+function App() {
+
+
+    const [theme, setTheme] = useLocalStorage("theme" ? "dark" : "light")
+
+ const switchTheme = () => {
+    const newTheme = theme === "light" ? "dark" : "light";
+    setTheme(newTheme)
+  
+ }
+
+  return (
+    <div className="app"data-theme={theme} >
+       <div className='login'>
+        <h1>Login</h1>
+        <div className='container'>
+          <div className='top'>
+            <i class="fab fa-google"></i>
+            <i class="fab fa-facebook-square"></i>
+            <i class="fab fa-linkedin"></i>
+            <i class="fab fa-twitter-square"></i>
+            <i class="fab fa-apple"></i>
+          </div>
+          <p className='divider'> <span>Or</span></p>
+          <form>
+             <label>E-mail</label>
+             <input type="email" placeholder="Typing or email"  />
+             <label>Password</label>
+             <input type="password" placeholder="Typing or password"  />
+              <div className='remember'>
+                <input type="checkbox"  checked="checked" />
+                <p>Remember me</p>
+              </div>
+              <button>Log In</button>
+          </form>
+           <div className='bottom'>
+            <p>Forget your password?</p>
+            <a href='/'>Reset Password</a>
+           </div>
+           <p className='create' >Create new Account</p>
+        </div>
+        <div className='theme-toggle'>
+          <h2>Light Theme</h2>
+          <i  onClick={switchTheme}  class="fas fa-toggle-on"></i>
+        </div>
+       </div>
+    
+    </div>
+  );
+}
+
+export default App;
